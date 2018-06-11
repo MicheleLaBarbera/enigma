eaadd197579eb936ccd6f6b6bdb989ae927178d1
Michele La Barbera <mlabarbera@POSDATA.net>
2018-06-11 12:57:49 +0200

    Added name

--- src/app/_models/host.ts
+++ src/app/_models/host.ts
@@ -10,8 +10,9 @@ export class Host {
   ip: string;
   port: number;
   hard_state: number;
+  name_ex: string;
 
-  constructor(address: string, alias: string, groups: string, crit: string, ok:string, unknown: string, warn: string, name: string, ip:string, port: number, hard_state: number) {
+  constructor(address: string, alias: string, groups: string, crit: string, ok:string, unknown: string, warn: string, name: string, ip:string, port: number, hard_state: number, name_ex: string) {
   	this.address = address;
   	this.alias = alias;
   	this.groups = groups;
@@ -23,5 +24,6 @@ export class Host {
     this.ip = ip;
     this.port = port;
     this.hard_state = hard_state;
+    this.name_ex = name_ex;
   }
 }
